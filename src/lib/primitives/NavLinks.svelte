261f86bab8e407be55b7b29bcd664dbc884437cf
Wilhelm Werwein <werwein.dev@gmail.com>
2023-10-05 11:48:17 +0200

    restructure layout

--- src/lib/primitives/NavLinks.svelte
+++ src/lib/primitives/NavLinks.svelte
@@ -4,13 +4,9 @@
     import Categories from './Categories.svelte';
 </script>
 
-
-<a href="/" class="flex items-center gap-0.5 text-lg hover:text-primary-500"><Icon icon="ic:outline-home" color="bg-surcace-50-900-token" width="20"/> Home</a>
-<details href="/" class="text-lg cursor-pointer md:hidden "><summary class="hover:text-primary-500">Categories</summary>
-    <div class="flex flex-col items-center gap-1">
-        <Categories />
-    </div>
-</details>
+<div class="flex flex-col items-center gap-1 md:hidden">
+    <Categories />
+</div>
 <!--Bottom Row-->
 <section class="flex items-center gap-2 p-4 card bg-surface-300-600-token hover:bg-surface-200-700-token">
     <a
