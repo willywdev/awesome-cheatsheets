ab259816a00ca8e488acf6b77ed3ac6e85a6d52a
Wilhelm Werwein <werwein.dev@gmail.com>
2023-10-14 12:08:51 +0200

    remove Breadcrumbs

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,10 +1,9 @@
 <script>
 // svelte docs: data prop that is given from +page.js
 export let data;
-import Breadcrumbs from '../../lib/primitives/Breadcrumbs.svelte';
 import changeStringFirstLetter from "$lib/utils/changeStringFirstLetter.js"
 
-const slug = changeStringFirstLetter(data.slug);
+$: slug = changeStringFirstLetter(data.slug);
 </script>
 
 <svelte:head>
@@ -12,5 +11,5 @@ const slug = changeStringFirstLetter(data.slug);
 	<meta name="description" content="Awesome Cheatsheets for Web Developers">
 </svelte:head>
 
-<Breadcrumbs slug={data}/>
+
 <h2>Hello!</h2>
