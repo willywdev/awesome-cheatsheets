b2dfc5154dda9b1c7560cd72c11326c07d36c558
Wilhelm Werwein <werwein.dev@gmail.com>
2023-10-14 12:41:04 +0200

    add underline and underline offset for h2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,8 +11,8 @@
     <div class="p-2">
         <img src={landingPageImage} alt="Landing Page" class="border-2 border-solid rounded-lg shadow-md border-primary-500" loading="lazy">
     </div>
-    <h2 class="mt-3 text-xl">Welcome to Awesome Cheatsheets!</h2>
+    <h2 class="mt-3 text-xl underline underline-offset-4">Welcome to Awesome Cheatsheets!</h2>
     <p class="mt-2 text-lg text-center">Unlock the power of web development with our curated collection of resources. Whether you're diving into HTML, CSS, React or exploring the vast world of JavaScript frameworks, we've got you covered.</p>
     <p class="mt-2 text-lg text-center">Navigate through our extensive categories to find handpicked cheatsheets, guides, and valuable links to accelerate your learning journey.</p>
-    <p class="mt-2 mb-2 text-lg text-center text-primary-400">Level up your skills, one cheat sheet at a time. Let's get started!</p>
+    <p class="mt-2 mb-5 text-lg text-center text-primary-400">Level up your skills, one cheat sheet at a time. Let's get started!</p>
 </section>
