fa42aea1894a925ca73b2bab374efe8d3070e1c3
Wilhelm Werwein <werwein.dev@gmail.com>
2023-10-14 12:39:00 +0200

    add Landing Page Image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-
+    import landingPageImage from "$lib/images/caspar-camille-rubin-0qvBNep1Y04-unsplash.jpg"
 </script>
 
 <svelte:head>
@@ -8,11 +8,11 @@
 </svelte:head>
 
 <section class="flex flex-col items-center ">
-    <div>
-
+    <div class="p-2">
+        <img src={landingPageImage} alt="Landing Page" class="border-2 border-solid rounded-lg shadow-md border-primary-500" loading="lazy">
     </div>
-    <h2 class="text-xl">Welcome to Awesome Cheatsheets!</h2>
+    <h2 class="mt-3 text-xl">Welcome to Awesome Cheatsheets!</h2>
     <p class="mt-2 text-lg text-center">Unlock the power of web development with our curated collection of resources. Whether you're diving into HTML, CSS, React or exploring the vast world of JavaScript frameworks, we've got you covered.</p>
     <p class="mt-2 text-lg text-center">Navigate through our extensive categories to find handpicked cheatsheets, guides, and valuable links to accelerate your learning journey.</p>
-    <p class="mt-2 text-lg text-center text-primary-400">Level up your skills, one cheat sheet at a time. Let's get started!</p>
+    <p class="mt-2 mb-2 text-lg text-center text-primary-400">Level up your skills, one cheat sheet at a time. Let's get started!</p>
 </section>
