75b6dc89e0541d4cb5fffb527a07008d9891907e
Wilhelm Werwein <werwein.dev@gmail.com>
2023-10-14 12:19:56 +0200

    create Landing Page Text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,5 +7,8 @@
 </svelte:head>
 
 <section class="flex flex-col items-center ">
-    <h2>Hello World!</h2>
+    <h2 class="text-xl">Welcome to Awesome Cheatsheets!</h2>
+    <p class="mt-2 text-lg text-center">Unlock the power of web development with our curated collection of resources. Whether you're diving into HTML, CSS, React or exploring the vast world of JavaScript frameworks, we've got you covered.</p>
+    <p class="mt-2 text-lg text-center">Navigate through our extensive categories to find handpicked cheatsheets, guides, and valuable links to accelerate your learning journey.</p>
+    <p class="mt-2 text-lg text-center text-primary-400">Level up your skills, one cheat sheet at a time. Let's get started!</p>
 </section>
