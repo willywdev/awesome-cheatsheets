3db4c5b053d86648d30c46ccd91ab7b098022395
Wilhelm Werwein <werwein.dev@gmail.com>
2023-10-17 08:32:22 +0200

    add padding to landing page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,8 +12,7 @@
     <div class="p-2 mt-2">
         <img src={landingPageImage} alt="Landing Page" class="rounded-lg shadow-lg border-2 border-surface-100 lg:w-[600px] lg:h-auto" loading="lazy">
     </div>
-    <h2 class="mt-3 text-xl font-bold text-center text-primary-400" >Welcome to Awesome Cheatsheets!</h2>
-    <p class="mt-2 text-lg">Unlock the power of web development with our curated collection of resources. Whether you're diving into HTML, CSS, React or exploring the vast world of JavaScript frameworks, we've got you covered.</p>
-    <p class="mt-2 text-lg">Navigate through our extensive categories to find handpicked cheatsheets, guides, and valuable links to accelerate your learning journey.</p>
-    <p class="mt-3 mb-5 text-lg">Level up your skills, one cheat sheet at a time. Let's get started!</p>
+    <h2 class="mt-3 mb-1 text-xl font-bold text-center text-primary-400" >Welcome to Awesome Cheatsheets!</h2>
+    <p class="mt-2 mb-1 text-lg">Unlock the power of web development with our curated collection of resources. Whether you're diving into HTML, CSS, React or exploring the vast world of JavaScript frameworks, we've got you covered.</p>
+    <p class="mt-2 mb-2 text-lg">Navigate through our extensive categories to find handpicked cheatsheets, guides, and valuable links to accelerate your learning journey.</p>
 </section>
