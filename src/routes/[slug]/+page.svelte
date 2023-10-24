62dbaa9cf716cfd6d1b2eabbaf548bc46812c943
willywdev <werwein.dev@gmail.com>
2023-10-24 11:07:21 +0200

    edit card styling

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -14,10 +14,10 @@ $: sheet = data.sheet
 <h2 class="mt-3 mb-1 text-xl font-bold text-center text-primary-400" >{slug.toUpperCase()}</h2>
 <CardContainer>
 		{#each sheet as item}
-			<a href={item.url} target="_blank" class="block max-w-sm p-6 border rounded-lg shadow border-surface-300 bg-surface-100 hover:bg-gray-100 dark:bg-surface-700 dark:border-surface-500 dark:hover:bg-surface-500">
+			<a href={item.url} target="_blank" class="block max-w-sm p-4 border rounded-lg shadow border-surface-300 bg-surface-100 hover:bg-gray-100 dark:bg-surface-700 dark:border-surface-500 dark:hover:bg-surface-500">
 				<li>
-					<h3 class="mb-2 text-xl font-bold tracking-tight">{item.title}</h3>
-					<p>{item.description}</p>
+					<h3 class="mb-1 text-lg font-bold tracking-tight">{item.title}</h3>
+					<p class="font-normal text-gray-700 dark:text-gray-400">{item.description}</p>
 				</li>
 			</a>
 		{/each}
