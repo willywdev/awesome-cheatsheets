2ed668b5395e605720ebc2ce5985b48caaec2e86
willywdev <werwein.dev@gmail.com>
2023-10-31 12:37:47 +0100

    implement clicking outside navbar closes the navbar

--- src/lib/primitives/HamburgerMenu.svelte
+++ src/lib/primitives/HamburgerMenu.svelte
@@ -9,11 +9,13 @@
    <Hamburger --color="white" bind:open/>
 </div>
 {#if open}
-   <div class="fixed inset-0 z-10 flex items-center justify-end md:hidden">
-       <nav transition:slide={{
-         axis: "x"
-       }} class="flex flex-col items-center justify-end w-3/4 h-full gap-3 py-5 overflow-y-scroll shadow-2xl bg-surface-50-900-token z-5">
-           <NavLinks />
-       </nav>
+   <div class="w-full h-full" on:click={() => open = false} aria-label="close navbar">
+       <div class="fixed inset-0 z-10 flex items-center justify-end md:hidden">
+           <nav transition:slide={{
+             axis: "x"
+           }} class="flex flex-col items-center justify-end w-3/4 h-full gap-3 py-5 overflow-y-scroll shadow-2xl bg-surface-50-900-token z-5" on:click|stopPropagation>
+               <NavLinks />
+           </nav>
+       </div>
    </div>
 {/if}
