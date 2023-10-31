920eb87a74d44f694b93155c0100046adb5ec02b
willywdev <werwein.dev@gmail.com>
2023-10-31 12:06:00 +0100

    fix grid view on mobile

--- src/lib/primitives/CardContainer.svelte
+++ src/lib/primitives/CardContainer.svelte
@@ -1,3 +1,3 @@
-<ul class="grid w-full grid-cols-2 gap-5 p-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+<ul class="grid w-full grid-cols-1 gap-5 p-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
     <slot />
 </ul>
